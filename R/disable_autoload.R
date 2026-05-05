37f42dbda00959d2e6011abf901fad8d505f17ea
Lalit BC <89679878+Lalitgis@users.noreply.github.com>
2026-05-04 21:55:31 -0400

    Clarify purpose and disable Shiny autoload
    Add comments to clarify the purpose of the file and set the Shiny autoload option.

--- R/disable_autoload.R
+++ R/disable_autoload.R
@@ -1,1 +1,4 @@
-# This file disables Shiny's automatic R/ folder loading.
+# disable_autoload.R
+# Presence of this file tells Shiny NOT to auto-source R/ alphabetically.
+# The actual option is also set in app.R for belt-and-suspenders reliability.
+options(shiny.autoload.r = FALSE)
